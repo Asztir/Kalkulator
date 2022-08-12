07a17e623c9ef5457b3b691d2dc2f03eae6075f0
asztir <asztir@knycz.net>
2022-08-12 11:29:47 +0200

    calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,27 @@
-package kalkulator;
+package calculator;
 
-public class ProstyKalkulator {
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in) ;
+        System.out.println("Podaj liczbę naturalną a");
+        int aNumber = scanner.nextInt();
+        System.out.println("Podaj liczbę naturalną b");
+        int bNumber = scanner.nextInt();
+
+        int addition = aNumber + bNumber;
+        int subtraction = aNumber - bNumber;
+        int multiplication = aNumber * bNumber;
+        float division = aNumber / bNumber;
+        int modulo = aNumber%bNumber;
+
+        System.out.println("Dodawanie " + addition);
+        System.out.println("Odejmowanie " + subtraction);
+        System.out.println("Mnożenie " + multiplication);
+        System.out.println("Dzielenie " + division);
+        System.out.println("Reszta z dzielenia " + modulo);
+
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in) ;
+        System.out.println("Podaj liczbę a");
     }
 }
